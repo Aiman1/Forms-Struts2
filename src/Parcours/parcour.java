2dc1f05a06bc9f010c3aa02e1d57f74fa1d7af8d
zakiaima <aiman.zaki@etu.utc.fr>
2019-03-19 16:51:54 +0100

    Initial commit

---
+++ src/Parcours/parcour.java
@@ -0,0 +1,16 @@
+package Parcours;
+
+import Questionnaire.questionnaire;
+import Questionnaire.réponse;
+import Utilisateurs.Utilisateur;
+
+import java.util.List;
+
+public class parcour {
+    private Utilisateur u;
+    private questionnaire q;
+    private double dateDébut;
+
+    private List<réponse> réponsesDonnées;
+
+}
---
+++ src/Questionnaire/réponse.java
@@ -0,0 +1,8 @@
+package Questionnaire;
+
+public abstract class réponse {
+    private Boolean statut;
+    private String réponse;
+    private Boolean bonne;
+
+}
