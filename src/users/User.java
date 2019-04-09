fc0ee6e552eb46bf5b235008b131484ca531f9d8
zakiaima <aiman.zaki@etu.utc.fr>
2019-04-09 17:39:12 +0200

    added image

--- src/users/User.java
+++ src/users/User.java
@@ -1,10 +1,24 @@
 package users;
 
 public class User {
+    String user_familly_name;
+    String user_first_name;
+    String user_email;
+    int size;
+    String gender;
+    String user_password;
+
     public User(String user_familly_name, String user_first_name, String user_email, int size, String gender, String user_password) {
+        this.user_familly_name = user_familly_name;
+        this.user_first_name = user_first_name;
+        this.user_email = user_email;
+        this.size = size;
+        this.gender = gender;
+        this.user_password = user_password;
     }
+
     @Override
     public String toString(){
-        return null;
+        return "User first name = " + user_first_name + "User familly name = " + user_familly_name + "User email = " + user_email + "User gender = " + gender + "User pwd = " + user_password;
     }
 }
