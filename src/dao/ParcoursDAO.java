083a90e7491973ab1380cae7917e36a369b40381
Jérémie Levet <jeremielevet@gmail.com>
2019-06-11 19:18:24 +0200

    parcours

--- src/dao/ParcoursDAO.java
+++ src/dao/ParcoursDAO.java
@@ -17,31 +17,93 @@ public class ParcoursDAO implements DAO<Parcours>{
 
 	@Override
 	public Optional<Parcours> get(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Parcours parcours = new Parcours();
+		try {
+			Statement sql = db.createStatement();
+			String sqlText = "SELECT * FROM Parcours WHERE id = " + id;
+			ResultSet res = sql.executeQuery(sqlText);
+			if(res.next()) {
+				questionnaire = new Parcours((int)id, res.getDouble("duree"), res.getInt("score"), res.getInt("idUtilisateur"), res.getInt("idQuestionnaire"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return Optional.ofNullable(questionnaire);
 	}
 
 	@Override
 	public List<Parcours> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Parcours> l = new ArrayList<Parcours>();
+		try {
+			Statement st = db.createStatement();
+			ResultSet res = st.executeQuery("select * from Parcours" );
+
+			while(res.next()){
+				l.add(new Parcours((int)id, res.getDouble("duree"), res.getInt("score"), res.getInt("idUtilisateur"), res.getInt("idQuestionnaire")););
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return l;
 	}
 
 	@Override
 	public int create(Parcours t) {
-		// TODO Auto-generated method stub
-		return 0;
+		try{
+			Statement sql = db.createStatement();
+			String query = " insert into Parcours (duree, score, idUtilisateur, idQuestionnaire)"
+					+ " values (?, ?, ?, ?)";
+
+			PreparedStatement preparedStmt = db.prepareStatement(query);
+			preparedStmt.setString(1, t.getIntitule());
+			preparedStmt.setBoolean(2, t.getStatut());
+			preparedStmt.setInt(4, t.getIdQuestionnaire());
+			preparedStmt.setInt(3, t.getIdUtilisateur());
+
+			preparedStmt.execute();
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return 0;
+		}
+		return 1;
 	}
 
 	@Override
 	public int update(Parcours t, String[] params) {
-		// TODO Auto-generated method stub
+		try{
+			String query = " update Parcours set (duree = ?, score = ?, idUtilisateur = ?, idQuestionnaire = ?)";
+
+			PreparedStatement preparedStmt = db.prepareStatement(query);
+			preparedStmt.setString(1, t.getIntitule());
+			preparedStmt.setBoolean(2, t.getStatut());
+			preparedStmt.setInt(4, t.getIdQuestionnaire());
+			preparedStmt.setInt(3, t.getIdUtilisateur());
+
+			preparedStmt.execute();
+		}
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
 		return 0;
 	}
 
 	@Override
 	public int delete(Parcours t) {
-		// TODO Auto-generated method stub
+		try {
+			Statement sql = db.createStatement();
+			String sqlText = "DELETE FROM Parcours WHERE id = " + t.getId();
+			ResultSet res = sql.executeQuery(sqlText);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return 0;
 	}
 	
--- src/parcours/Parcours.java
+++ src/parcours/Parcours.java
@@ -7,7 +7,54 @@ import utilisateurs.Utilisateur;
 import java.util.List;
 
 public class Parcours {
-    private Utilisateur u;
-    private Questionnaire q;
-    private double dateDebut;
+    private int id;
+    private double duree;
+    private int score;
+    private int idUtilisateur;
+    private int idQuestionnaire;
+    private List<Reponse> reponses;
+
+    public Parcours(){
+
+    }
+
+    public Parcours(int id, double duree, int score, int idUtilisateur, int idQuestionnaire) {
+        this.id = id;
+        this.idUtilisateur = idUtilisateur;
+        this.idQuestionnaire = idQuestionnaire;
+        this.duree = duree;
+        this.score = score;
+    }
+
+    public Utilisateur getU() {
+        return u;
+    }
+
+    public void setU(Utilisateur u) {
+        this.u = u;
+    }
+
+    public Questionnaire getQ() {
+        return q;
+    }
+
+    public void setQ(Questionnaire q) {
+        this.q = q;
+    }
+
+    public double getDuree() {
+        return duree;
+    }
+
+    public void setDuree(double duree) {
+        this.duree = duree;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
